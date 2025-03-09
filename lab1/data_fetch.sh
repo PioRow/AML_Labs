722797a141b07ee93f698aa48afb4609aadad43c
PioRow <piotr.rowicki02@gmail.com>
2025-03-09 13:01:08 +0100

    density estimation prep

---
+++ lab1/data_fetch.sh
@@ -0,0 +1,3 @@
+curl  https://www.openml.org/data/download/53353/delta_elevators.arff -o dataset1.arff
+curl  https://api.openml.org/data/download/22111914/dataset -o dataset2.arff
+curl  https://www.openml.org/data/download/1586225/php0iVrYT -o dataset3.arff
